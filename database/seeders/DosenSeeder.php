0df9c7ee2ed9b6bbcd333b190b013a80f55a15be
nzwanbla <nzwnabila@gmail.com>
2024-04-25 23:22:47 +0700

    update migration, seeder, and factory dosen

---
+++ database/seeders/DosenSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Dosen;
+
+class DosenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Dosen::create([
+            'nama' => 'John Doe',
+            'gelar' => 'PhD'
+        ]);
+
+        Dosen::create([
+            'nama' => 'Jane Smith',
+            'gelar' => 'MSc'
+        ]);
+    }
+}
